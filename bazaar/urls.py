096365a247da821377e6f769d71a31c91726c484
roy-eli-wal <alihassan07official@gmail.com>
2024-09-28 09:02:47 +1000

    Add new comment

--- bazaar/urls.py
+++ bazaar/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+#commmeents
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('playground/',include('playground.urls'))
